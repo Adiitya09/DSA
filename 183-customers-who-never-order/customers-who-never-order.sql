73297dec2e8e37f88a4f9e303580e68e72d2759a
Adiitya09 <2004aditypatil@gmail.com>
2026-08-25 23:08:12 +0530

    Delete 183-customers-who-never-order directory

--- 183-customers-who-never-order/customers-who-never-order.sql
+++
@@ -1,5 +0,0 @@
-# Write your MySQL query statement below
-select Customers.name as Customers from Customers left join 
-Orders on(Customers.id = Orders.customerId)
-where Orders.customerId is null;
- 
