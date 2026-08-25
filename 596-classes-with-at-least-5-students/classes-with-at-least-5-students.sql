8ef6e5b216bce1c77dbdbd62156c58bb84866b07
Adiitya09 <2004aditypatil@gmail.com>
2026-08-25 23:08:29 +0530

    Delete 596-classes-with-at-least-5-students directory

--- 596-classes-with-at-least-5-students/classes-with-at-least-5-students.sql
+++
@@ -1,7 +0,0 @@
-# Write your MySQL query statement below
-with Course as (
-     select class , count(class) as count 
-    from Courses group by class
-)
-select class from Course
-Where count >=5;
